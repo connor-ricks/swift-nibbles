aac717c47a593f5f0dbbe9b68197c9885b37e269
Connor Ricks <connor.ricks@gmail.com>
2023-07-23 11:19:01 -0400

    🛠  Rename Scripting to Utilities

--- Utilities/setup.sh
+++ Utilities/setup.sh
@@ -2,6 +2,6 @@
 
 echo "🛠️ Setting up local git hooks..."
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
-git config --local core.hooksPath "${PROJECT_DIR}/Scripting/Hooks/"
-chmod u+x "${PROJECT_DIR}/Scripting/Hooks/commit-msg"
+git config --local core.hooksPath "${PROJECT_DIR}/Utilities/Hooks/"
+chmod u+x "${PROJECT_DIR}/Utilities/Hooks/commit-msg"
 echo "✅ Local git hooks setup."
